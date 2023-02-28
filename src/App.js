d0221e2b24930722c9e7cb5b8d60a8b2d671b6fd
Aarun1349 <arunmahendra90@gmail.com>
2023-02-28 11:58:33 +0530

    Home Header Footer MetaData using Helmet , router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-
-import './App.css';
-
+import "./App.css";
+import { Home } from "./components/Home";
+import Footer from "./components/layouts/Footer";
+import Header from "./components/layouts/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { MetaData } from "./components/layouts/MetaData";
 function App() {
   return (
-    <div>
-    Welcome to Shop-IT 
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <div className="conatiner container-fluid">
+          <Routes>
+            <Route path="/" exact element={<Home />}></Route>
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
